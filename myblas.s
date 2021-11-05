47aa7c595f85d98d6a886411f58498971fbc9268
Maite Gonzalez <32800603+mgonzalez132@users.noreply.github.com>
2021-11-05 10:47:37 -0700

    Add files via upload

---
+++ myblas.s
@@ -0,0 +1,52 @@
+	.file	"myblas.c"
+	.text
+	.globl	dewvm
+	.type	dewvm, @function
+dewvm:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -20(%rbp)
+	movq	%rsi, -32(%rbp)
+	movq	%rdx, -40(%rbp)
+	movq	%rcx, -48(%rbp)
+	movl	$0, -4(%rbp)
+	jmp	.L2
+.L3:
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	0(,%rax,8), %rdx
+	movq	-48(%rbp), %rax
+	addq	%rdx, %rax
+	movl	-4(%rbp), %edx
+	movslq	%edx, %rdx
+	leaq	0(,%rdx,8), %rcx
+	movq	-32(%rbp), %rdx
+	addq	%rcx, %rdx
+	movupd	(%rdx), %xmm1
+	movl	-4(%rbp), %edx
+	movslq	%edx, %rdx
+	leaq	0(,%rdx,8), %rcx
+	movq	-40(%rbp), %rdx
+	addq	%rcx, %rdx
+	movupd	(%rdx), %xmm0
+	mulpd	%xmm1, %xmm0
+	movupd	%xmm0, (%rax)
+	addl	$2, -4(%rbp)
+.L2:
+	movl	-4(%rbp), %eax
+	cmpl	-20(%rbp), %eax
+	jl	.L3
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	dewvm, .-dewvm
+	.ident	"GCC: (Debian 6.3.0-18+deb9u1) 6.3.0 20170516"
+	.section	.note.GNU-stack,"",@progbits
